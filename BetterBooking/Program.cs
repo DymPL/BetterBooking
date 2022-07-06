355211250fd9aa4bc6735376db8bc4fa893f94e6
Dymek <d.rzepiejewski@gmail.com>
2022-07-07 00:05:01 +0200

    better booking menu

--- BetterBooking/Program.cs
+++ BetterBooking/Program.cs
@@ -8,12 +8,74 @@ namespace BetterBooking
         static void Main(string[] args)
         {
             Console.WriteLine("Witamy na BetterBooking");
-            Console.WriteLine("Właściciel hotelu / Turysta");
+            //Console.WriteLine("Właściciel hotelu / Turysta");
 
-            Console.WriteLine("1. Dodaj hotel");
-            Console.WriteLine("2. Usuń hotel");
+            Console.WriteLine("1. Właściciel hotelu \n2. Turysta");
 
-            Console.WriteLine("1. Lokalizacja");
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Wybrałeś opcje: {choice}");
+
+            ItemService itemService = new ItemService();
+
+            if (choice == 1)
+            {
+                Console.WriteLine("1. Dodaj hotel \n2. Info hotel \n3. Usuń hotel");
+                choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    Console.WriteLine("Wybrałeś: Dodaj hotel");
+                    // Owners owners = new Owners() { Id = 1, Name = "Hotel JEDEN"};
+                    itemService.AddItem();
+                }
+                else if (choice == 2)
+                {
+                    Console.WriteLine("Wybrałeś: Info hotel");
+                }
+                else if(choice == 3)
+                {
+                    Console.WriteLine("Wybrałeś: Usuń hotel");
+                    itemService.RemoveItem();
+                }
+                else
+                {
+                    Console.WriteLine("zamkniujsda sdsa");
+                }
+
+            } else if (choice == 2)
+                    {
+                Console.WriteLine("1. Lokalizacja \n2. Standard - VIP, Standard, Biedak \n3. Cena \n4. Pokoje \n5. Dostępność w czasie");
+                choice = int.Parse(Console.ReadLine());
+                if(choice == 1)
+                {
+                    Console.WriteLine("1. Lokalizacja");
+                }
+                else if (choice == 2) {
+                    Console.WriteLine("2. Standard - VIP, Standard, Biedak");
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("3. Cena");
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine("3. Pokoje");
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("5. Dostępność w czasie");
+                }
+                else
+                {
+                    Console.WriteLine("duap");
+                }
+            }
+                            
+            else {
+                Console.WriteLine("Zamknij program");
+            }
+
+            /*Console.WriteLine("1. Lokalizacja");
             Console.WriteLine("2. Standard - VIP, Standard");
             Console.WriteLine("3. Cena");
             Console.WriteLine("4. Pokoje");
@@ -22,11 +84,15 @@ namespace BetterBooking
             Console.WriteLine("1. Potwierdzenie rezerwacji");
             Console.WriteLine("2. Płatność / zaliczki");
             Console.WriteLine("3. Oceny hotelu");
-            Console.WriteLine("4. Anulowanie rezerwacji");
+            Console.WriteLine("4. Anulowanie rezerwacji");*/
+            
 
-            int choice = int.Parse(Console.ReadLine());
 
-            Console.WriteLine($"Wybrałeś opcje: {choice}");
+
+
+            //  Owners owners = new Owners() { Id = 1, Name = "Hotel Jeden" };
+
+            //Console.WriteLine(owners.Id);
 
         }
 
--- BetterBooking/Reservation.cs
+++ BetterBooking/Reservation.cs
@@ -6,23 +6,13 @@ using System.Threading.Tasks;
 
 namespace BetterBooking
 {
-    public class Owners
+    public class Reservation
     {
-        public int Id { get; set; } 
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Localization { get; set; }
         public int Standard { get; set; }
         public int Price { get; set; }
         public int Date { get; set; }
-
-    }
-    public class ItemService
-    {
-        public void AddItem()
-        { }
-        public void RemoveItem()
-        {
-
-        }
     }
 }
