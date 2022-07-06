816c8df5ab0bf9c18f7bff8a1e98cfb9db4d699c
Dymek <d.rzepiejewski@gmail.com>
2022-07-06 22:41:29 +0200

    better booking

---
+++ BetterBooking/Owners.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterBooking
+{
+    public class Owners
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int Localization { get; set; }
+        public int Standard { get; set; }
+        public int Price { get; set; }
+        public int Date { get; set; }
+
+    }
+}
---
+++ BetterBooking/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BetterBooking
+{
+    class Program
+    {
+        public const string DATABASE = "database.txt";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witamy na BetterBooking");
+            Console.WriteLine("Właściciel hotelu / Turysta");
+
+            Console.WriteLine("1. Dodaj hotel");
+            Console.WriteLine("2. Usuń hotel");
+
+            Console.WriteLine("1. Lokalizacja");
+            Console.WriteLine("2. Standard - VIP, Standard");
+            Console.WriteLine("3. Cena");
+            Console.WriteLine("4. Pokoje");
+            Console.WriteLine("5. Dostępność w czasie");
+
+            Console.WriteLine("1. Potwierdzenie rezerwacji");
+            Console.WriteLine("2. Płatność / zaliczki");
+            Console.WriteLine("3. Oceny hotelu");
+            Console.WriteLine("4. Anulowanie rezerwacji");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Wybrałeś opcje: {choice}");
+
+        }
+
+    }
+}
